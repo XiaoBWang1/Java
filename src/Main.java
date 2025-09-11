76e65c0e4e1f4eecfcdd5d1e7db6661bf61d74d8
xbw24 <xbw24@yahoo.com>
2025-09-11 11:14:14 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+/*' Java primitive(stack) data types: int, double, char, boolean(true/false)
+    Java Reference(heap) data types: String, array, object
+    System.out.println|print("Output"),
+    import java.util.scanner/ Scanner scanner = new Scanner(System.in);/ scanner.close();/ scanner.next()|nextLine()|nextInt();
+    if, else, elif,
+    declaration, assignment
+ */
+
+public class Main{
+    public static void main(String[] args){
+        System.out.println("Java was used to be called Oak!");
+    }}
